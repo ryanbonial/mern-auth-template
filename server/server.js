d6e3419256996415a14fedad80c4e105dd3409f2
Ryan Bonial <ryanbonial@gmail.com>
2020-05-24 13:38:52 -0600

    Adds user creation and login starter

--- server/server.js
+++ server/server.js
@@ -1,8 +1,43 @@
 const express = require('express');
+const argon2 = require('argon2');
 
 const app = express();
+app.use(express.json())
+const users = [];
 
 app.get('/ping', (req, res) => res.send('pong'));
 
-console.log('Server listening on http://127.0.0.1:4000/ping')
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+    const { name, password } = req.body
+    try {
+        const passwordHash = await argon2.hash(password, 10);
+        users.push({ name, passwordHash });
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { name, password } = req.body;
+    const user = users.find(user => user.name === name);
+    if (!user) {
+        return res.status(401).send('Invalid username or password');
+    }
+    try {
+        if (await argon2.verify(user.passwordHash, password)) {
+            return res.status(200).send(user);
+        } else {
+            return res.status(401).send('Invalid username or password');
+        }
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+console.log('Server listening on http://localhost:4000/ping')
 app.listen(4000);
