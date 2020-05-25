0f7c0ca53a2864733cad4a261c023c06c16062aa
Ryan Bonial <ryanbonial@gmail.com>
2020-05-24 20:01:59 -0600

    Fixes split header bug

--- server/auth/authGuard.js
+++ server/auth/authGuard.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'].split(' ')[1];
+    const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : '';
     try {
         jwt.verify(token, process.env.AUTH_TOKEN_SECRET, { algorithms: ['HS256'] });
         next();
