84b6cb1c097496bd5f896b8ec34c95d7406ca299
Ryan Bonial <ryanbonial@gmail.com>
2020-05-26 16:23:58 -0600

    Adds ESLint to Server

--- server/auth/authGuard.js
+++ server/auth/authGuard.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
+        // eslint-disable-next-line dot-notation
         const token = req.headers['authorization'].split(' ')[1];
         jwt.verify(token, process.env.AUTH_TOKEN_SECRET, { algorithms: ['HS256'] });
         next();
--- server/auth/authToken.js
+++ server/auth/authToken.js
@@ -2,21 +2,23 @@ const jsonwebtoken = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
 
 const getAuthToken = function (user) {
-    return jsonwebtoken.sign({
-        user: { name: user.name }
-    },
+    return jsonwebtoken.sign(
+        {
+            user: { name: user.name }
+        },
         process.env.AUTH_TOKEN_SECRET,
-        { expiresIn: "10m" }
+        { expiresIn: '10m' }
     );
 }
 
 const getRefreshToken = function (user) {
-    return jsonwebtoken.sign({
-        user: { name: user.name }
-    },
+    return jsonwebtoken.sign(
+        {
+            user: { name: user.name }
+        },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: "7d",
+            expiresIn: '7d',
             jwtid: uuidv4()
         }
     );
