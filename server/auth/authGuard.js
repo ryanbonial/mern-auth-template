58c9dd91e8fba1b81b66242d090b947e636c703e
Ryan Bonial <ryanbonial@gmail.com>
2020-05-26 08:54:45 -0600

    Clean up error handling

--- server/auth/authGuard.js
+++ server/auth/authGuard.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : '';
     try {
+        const token = req.headers['authorization'].split(' ')[1];
         jwt.verify(token, process.env.AUTH_TOKEN_SECRET, { algorithms: ['HS256'] });
         next();
     } catch {
-        res.status(401).json({ error: new Error('Invalid authorization token') });
+        res.status(401).json({ error: 'Invalid authorization token' });
     }
 };
