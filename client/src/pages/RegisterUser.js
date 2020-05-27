3d6001b7941b4d498f25d7921d706bb17b57ab4f
Ryan Bonial <ryanbonial@gmail.com>
2020-05-27 13:45:14 -0600

    Adds Register page

--- client/src/pages/RegisterUser.js
+++ client/src/pages/RegisterUser.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageContent from '../PageContent';
 
-export default function RegisterUser() {
-  return <PageContent>Register page</PageContent>
+export default function RegisterUser({history}) {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onRegisterSubmit = async e => {
+    e.preventDefault();
+    const regResp = await fetch('http://localhost:4000/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: userName, password }),
+    });
+    console.log(regResp);
+    history.push('/');
+  };
+
+  return (
+    <PageContent className="register">
+      <h2>Register</h2>
+      <form onSubmit={onRegisterSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </PageContent>
+  );
 }
